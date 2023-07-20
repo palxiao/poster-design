8a99924991cc14b094a714509825fae75947704b
ShawnPhang <palxiao@vip.qq.com>
2023-07-20 18:48:46 +0800

    code: delete old data

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@
  * @Date: 2021-08-19 18:30:38
  * @Description: Vite配置文件
  * @LastEditors: ShawnPhang <site: book.palxp.com>
- * @LastEditTime: 2023-07-15 15:31:49
+ * @LastEditTime: 2023-07-20 18:48:25
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -28,22 +28,6 @@ export default defineConfig({
     ElementPlus({
       // options
     }),
-    // styleImport({
-    //   libs: [
-    //     {
-    //       libraryName: 'element-plus',
-    //       esModule: true,
-    //       ensureStyleFile: true,
-    //       resolveStyle: (name) => {
-    //         name = name.slice(3)
-    //         return `element-plus/packages/theme-chalk/src/${name}.scss`
-    //       },
-    //       resolveComponent: (name) => {
-    //         return `element-plus/lib/${name}`
-    //       },
-    //     },
-    //   ],
-    // }),
   ],
   build: {
     minify: 'terser',
@@ -76,7 +60,7 @@ export default defineConfig({
     hmr: { overlay: false },
     // proxy: {
     //   '/api': {
-    //     target: 'https://rmt-design-dev.imp360.cn/api/',
+    //     target: '',
     //     changeOrigin: true,
     //     rewrite: (path) => path.replace(/^\/api/, ''),
     //   },
