d2f171e3687c929abd9c63067802d80137abc2cb
ShawnPhang <palxiao@vip.qq.com>
2023-10-05 00:20:02 +0800

    code: update photos

--- src/components/modules/panel/wrap/components/imageTip.vue
+++ src/components/modules/panel/wrap/components/imageTip.vue
@@ -3,10 +3,10 @@
  * @Date: 2023-10-04 19:12:40
  * @Description: 图片描述ToolTip
  * @LastEditors: ShawnPhang <https://m.palxp.cn>
- * @LastEditTime: 2023-10-04 22:21:22
+ * @LastEditTime: 2023-10-04 22:51:06
 -->
 <template>
-  <el-tooltip :offset="1" effect="light" placement="bottom-start" :hide-after="0" :enterable="false" raw-content>
+  <el-tooltip :disabled="!detail.author" :offset="1" effect="light" placement="bottom-start" :hide-after="0" :enterable="false" raw-content>
     <template #content>
       <p style="max-width: 140px">
         <b>{{ detail.description }}</b>
