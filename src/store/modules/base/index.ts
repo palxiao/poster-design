75358f1c41744fbd3b288cd069ff102a2cb8e3d6
IchliebedichZhu <54796446@qq.com>
2024-03-17 16:53:16 +0000

    rebuilt: base store to pinia

--- src/store/modules/base/index.ts
+++ src/store/modules/base/index.ts
@@ -2,48 +2,86 @@
  * @Author: ShawnPhang
  * @Date: 2021-12-16 16:20:16
  * @Description:
- * @LastEditors: ShawnPhang <https://m.palxp.cn>
- * @LastEditTime: 2023-09-28 17:42:25
+ * @LastEditors: ShawnPhang <https://m.palxp.cn>, Jeremy Yu <https://github.com/JeremyYu-cn>
+ * @LastEditTime: 2024-03-17 15:00:00
  */
-import mutations from './mutations'
-import actions from './actions'
-import _config from '@/config'
 
-const all = {
-  state: {
+import { defineStore } from 'pinia'
+
+// import actions from './actions'
+// import _config from '@/config'
+
+type TStoreBaseState = {
+  loading: boolean | null
+  scroll: boolean
+  /** fonts */
+  fonts: string[]
+  /** 抠图服务 */
+  app: string | null
+}
+
+type TUserAction = {
+  hideLoading: () => void
+  setFonts: (list: string[]) => void
+}
+
+const useBaseStore = defineStore<'base', TStoreBaseState, {}, TUserAction>('base', {
+  state: () => ({
     loading: null,
-    online: true, // 登录状态，
-    user: {
-      name: localStorage.getItem('username'),
-    }, // 储存用户信息
     scroll: true,
-    manager: '', // 是否为管理员模式
-    tempEditing: false, // 管理员是否正在编辑模板
     fonts: [], // 缓存字体列表
     app: null, // 抠图服务
-  },
-  getters: {
-    online: (state: Type.Object) => {
-      return state.online
-    },
-    user: (state: Type.Object) => {
-      return state.user
-    },
-    manager: (state: Type.Object) => {
-      return state.manager
-    },
-    tempEditing: (state: Type.Object) => {
-      return state.tempEditing
-    },
-    fonts: (state: Type.Object) => {
-      return state.fonts
-    },
-  },
-  mutations: {
-    ...mutations,
-  },
+  }),
   actions: {
-    ...actions,
-  },
-}
-export default all
+    /** 隐藏loading */
+    hideLoading() {
+      setTimeout(() => {
+        this.loading = false
+      }, 600)
+    },
+    setFonts(list: string[]) {
+      this.fonts = list
+    },
+  }
+})
+
+export default useBaseStore
+
+// const all = {
+//   state: {
+//     loading: null,
+//     online: true, // 登录状态，
+//     user: {
+//       name: localStorage.getItem('username'),
+//     }, // 储存用户信息
+//     scroll: true,
+//     manager: '', // 是否为管理员模式
+//     tempEditing: false, // 管理员是否正在编辑模板
+//     fonts: [], // 缓存字体列表
+//     app: null, // 抠图服务
+//   },
+//   getters: {
+//     online: (state: Type.Object) => {
+//       return state.online
+//     },
+//     user: (state: Type.Object) => {
+//       return state.user
+//     },
+//     manager: (state: Type.Object) => {
+//       return state.manager
+//     },
+//     tempEditing: (state: Type.Object) => {
+//       return state.tempEditing
+//     },
+//     fonts: (state: Type.Object) => {
+//       return state.fonts
+//     },
+//   },
+//   mutations: {
+//     ...mutations,
+//   },
+//   actions: {
+//     ...actions,
+//   },
+// }
+
--- src/store/modules/base/mutations.ts
+++ src/store/modules/base/mutations.ts
@@ -8,35 +8,35 @@
 
 // import { Toast } from 'vant'
 
-export default {
-  loading(state: Type.Object, data: any) {
-    // Toast.clear();
-    // let msg = ''
-    // if (typeof data === 'string') {
-    //     msg = data
-    // } else {
-    //     Toast.clear();
-    //     return false
-    // }
-    // Toast.loading({
-    //     duration: 0,       // 持续展示 toast
-    //     loadingType: 'spinner',
-    //     message: msg
-    // });
-  },
-  changeRoute(state: Type.Object, from: string) {
-    state.routeFrom = from
-  },
-  changeOnline(state: Type.Object, status: string) {
-    state.online = status
-  },
-  changeUser(state: Type.Object, name: string) {
-    state.user.name = name
-    // state.user = Object.assign({}, state.user)
-    state.user = { ...state.user }
-    localStorage.setItem('username', name)
-  },
-  managerEdit(state: Type.Object, status: string) {
-    state.tempEditing = status
-  },
-}
+// export default {
+//   loading(state: Type.Object, data: any) {
+//     // Toast.clear();
+//     // let msg = ''
+//     // if (typeof data === 'string') {
+//     //     msg = data
+//     // } else {
+//     //     Toast.clear();
+//     //     return false
+//     // }
+//     // Toast.loading({
+//     //     duration: 0,       // 持续展示 toast
+//     //     loadingType: 'spinner',
+//     //     message: msg
+//     // });
+//   },
+//   changeRoute(state: Type.Object, from: string) {
+//     state.routeFrom = from
+//   },
+//   changeOnline(state: Type.Object, status: string) {
+//     state.online = status
+//   },
+//   changeUser(state: Type.Object, name: string) {
+//     state.user.name = name
+//     // state.user = Object.assign({}, state.user)
+//     state.user = { ...state.user }
+//     localStorage.setItem('username', name)
+//   },
+//   managerEdit(state: Type.Object, status: string) {
+//     state.tempEditing = status
+//   },
+// }
