ad37410987919cb8cffe8f79723e390363a74aae
1254107736@qq.com <125410736@qq.com>
2024-04-21 23:36:16 +0800

    添加动画效果2

--- src/components/business/animation/index.vue
+++ src/components/business/animation/index.vue
@@ -2,7 +2,8 @@
   <div>
     <!-- 选项模块 -->
     <div class="flex flex-wrap items-center mb-4">
-      <list :transition="params.transition" :drawer="drawer"></list>
+      <!-- <list :params="params" @openDrawer="openDrawer"></list> -->
+      <list2 v-bind='params' @openDrawer="openDrawer"></list2>
     </div>
 
     <!-- 右侧动画选择抽屉弹窗 -->
@@ -19,16 +20,17 @@
             <span>{{item.label}}</span>
           </div>
         </template>
-        <el-button size="small" v-for="(childrenItem, j) in item.children" :key="j" plain :class="['animateBtn', childrenItem.isAnimating ? 'animate__animated' : '', childrenItem.isAnimating ? 'animate__' + childrenItem.value : '']"  @mouseenter="animating(childrenItem, i, j)" @click="chooseAnimate(childrenItem, i, j)">{{childrenItem.label}}</el-button>
+        <el-button :type="childrenItem.value === params?.transition.animate ? 'primary' : ''" size="small" v-for="(childrenItem, j) in item.children" :key="j" plain :class="['animateBtn', childrenItem.isAnimating ? 'animate__animated' : '', childrenItem.isAnimating ? 'animate__' + childrenItem.value : '']"  @mouseenter="animating(childrenItem, i, j)" @click="chooseAnimate(childrenItem, i, j)">{{childrenItem.label}}</el-button>
       </el-card>
     </el-drawer>
   </div>
 </template>
 <script setup lang="ts">
-  import { ref, Ref } from 'vue'
+  import { ref, Ref, reactive } from 'vue'
   import {animationOptions} from './animate.js'
   import 'animate.css';
   import list from './list.vue';
+  import list2 from './list2.vue';
   import { ElDrawer } from 'element-plus'
   import type { DrawerProps } from 'element-plus'
   type TProps = {
@@ -51,7 +53,16 @@
       childrenItem.isAnimating = false;
     }, 1e3);
   }
-
+  const defaultTransition = reactive({
+  animate: '',
+  animateName: '',
+  duration: 0, 
+  delay: 0, 
+  speed: 'fast', 
+  isRepeat: false, 
+  repeatTime: 1, // 循环次数，1表示一次之后就停止
+})
+  let transition = reactive(props.params?.transition || defaultTransition)
   // 点击选中预览效果，画板添加预览显示
   function chooseAnimate(childrenItem, i, j){
     const { label, value } = childrenItem;
@@ -63,6 +74,11 @@
     // -- repeat: 重复次数
     // -- speed: 速度：slow, slower, fast, faster
     // -- isPreview： 是否预览，一次之后就停止,初始化设置的时候默认为true，发布作品时修改为false
+    // transition.value = {
+      console.log(props);
+      console.log(props.params);
+      console.log(props.params.transition);
+      
     props.params.transition = {
       animate: value,
       animateName: label,
@@ -73,12 +89,23 @@
       repeatTime: 1, // 循环次数，1表示一次之后就停止
       isPreview: true, // 是否预览，一次之后就停止
     }
+    console.log(props.params.transition);
+    console.log(transition.value);
+    
   }
 
   // 关闭抽屉弹窗
   function handleClose(){
     drawer.value = false;
     console.log("handleClose");
+    console.log(props.params)
+    console.log(props.params)
+  }
+
+  function openDrawer(){
+    console.log('openDrawer')
+    
+    drawer.value = true;
   }
 </script>
 
--- src/components/business/animation/list.vue
+++ src/components/business/animation/list.vue
@@ -1,134 +1,141 @@
 <template>
   <el-form
     ref="transitionRef"
-    style="max-width: 600px"
-    :model="transition"
+    style="width: 700px"
+    :model="params.transition"
     :rules="rules"
     label-width="auto"
     class="demo-transition"
     :size="formSize"
     status-icon
   >
-    <el-form-item label="动画" prop="animate">
-      <div @click="drawer = true">click me</div>
+    <el-form-item label="动画效果" prop="animate">
+      <div @click="$emit('openDrawer')">{{params.transition.animateName || '请选择'}}</div>
     </el-form-item>
     <el-form-item label="播放速度" prop="speed">
-      <el-segmented v-model="transition.speed" :options="speedOptions" />
+      <el-radio-group v-model="params.transition.speed" size="small">
+        <el-radio-button v-for="(item, i) in speedOptions" :key="i" :label="item.label" :value="item.value" />
+      </el-radio-group>
     </el-form-item>
     <!-- <el-form-item label="Activity zone" prop="region">
-      <el-select v-model="transition.region" placeholder="Activity zone">
+      <el-select v-model="params.transition.region" placeholder="Activity zone">
         <el-option label="Zone one" value="shanghai" />
         <el-option label="Zone two" value="beijing" />
       </el-select>
     </el-form-item> -->
     <el-form-item label="循环播放" prop="isRepeat">
-      <el-switch v-model="transition.isRepeat" />
+      <el-switch v-model="params.transition.isRepeat" />
     </el-form-item>
-    <el-form-item label="循环次数" prop="repeatTime">
-      <el-input-number v-model="transition.repeatTime" :min="1" :max="999" @change='getParams' />
+    <el-form-item label="循环次数" v-if="params.transition.isRepeat" prop="repeatTime">
+      <el-input-number v-model="params.transition.repeatTime" :min="1" :max="999" @change='getParams' />
     </el-form-item>
   </el-form>
 </template>
 
 <script lang="ts" setup>
 import { reactive, ref } from 'vue'
-import { ElForm, ElFormItem, ElInputNumber } from 'element-plus'
+import { ElForm, ElFormItem, ElInputNumber,ElRadioGroup, ElRadioButton } from 'element-plus'
 import type { ComponentSize, FormInstance, FormRules } from 'element-plus'
 
 type TProps = {
-    transition: any,
-    drawer: Boolean,
+    params: any,
   }
   const props = withDefaults(defineProps<TProps>(), {
-    transition: {},
-    drawer: false,
+    params: {},
   })
-// interface transition {
-//   animate: String,
-//   animateName: String,
-//   duration: Number, 
-//   delay: Number, 
-//   speed: String, 
-//   isRepeat: Boolean, 
-//   repeatTime: Number, // 循环次数，1表示一次之后就停止
-//   isPreview: Boolean, // 是否预览，一次之后就停止
-// }
+interface transition {
+  animate: String,
+  animateName: String,
+  duration: Number, 
+  delay: Number, 
+  speed: String, 
+  isRepeat: Boolean, 
+  repeatTime: Number, // 循环次数，1表示一次之后就停止
+  isPreview: Boolean, // 是否预览，一次之后就停止
+}
 
 const formSize = ref<ComponentSize>('default')
 const transitionRef = ref<FormInstance>()
-// const transition = reactive<transition>({
-//   animate: '',
-//   animateName: '',
-//   duration: 0, 
-//   delay: 0, 
-//   speed: 'fast', 
-//   isRepeat: false, 
-//   repeatTime: 1, // 循环次数，1表示一次之后就停止
-// })
-// transition.value = props.params.transition;
-const speedOptions = ['slow', 'slower', 'fast', 'faster']
+const defaultTransition = reactive<transition>({
+  animate: '',
+  animateName: '',
+  duration: 0, 
+  delay: 0, 
+  speed: 'fast', 
+  isRepeat: false, 
+  repeatTime: 1, // 循环次数，1表示一次之后就停止
+})
+let transition = reactive(props.params?.transition || defaultTransition)
 
-// const rules = reactive<FormRules<transition>>({
-//   name: [
-//     { required: true, message: 'Please input Activity name', trigger: 'blur' },
-//     { min: 3, max: 5, message: 'Length should be 3 to 5', trigger: 'blur' },
-//   ],
-//   region: [
-//     {
-//       required: true,
-//       message: 'Please select Activity zone',
-//       trigger: 'change',
-//     },
-//   ],
-//   count: [
-//     {
-//       required: true,
-//       message: 'Please select Activity count',
-//       trigger: 'change',
-//     },
-//   ],
-//   date1: [
-//     {
-//       type: 'date',
-//       required: true,
-//       message: 'Please pick a date',
-//       trigger: 'change',
-//     },
-//   ],
-//   date2: [
-//     {
-//       type: 'date',
-//       required: true,
-//       message: 'Please pick a time',
-//       trigger: 'change',
-//     },
-//   ],
-//   location: [
-//     {
-//       required: true,
-//       message: 'Please select a location',
-//       trigger: 'change',
-//     },
-//   ],
-//   type: [
-//     {
-//       type: 'array',
-//       required: true,
-//       message: 'Please select at least one activity type',
-//       trigger: 'change',
-//     },
-//   ],
-//   resource: [
-//     {
-//       required: true,
-//       message: 'Please select activity resource',
-//       trigger: 'change',
-//     },
-//   ],
-//   desc: [
-//     { required: true, message: 'Please input activity form', trigger: 'blur' },
-//   ],
-// })
+// params.transition.value = props.params.transition;
+const speedOptions = [
+  {value: 'slower', label: '最慢'},
+  {value: 'slow', label: '慢速'},
+  {value: 'fast', label: '快速'},
+  {value: 'faster', label: '最快'}
+]
+
+const rules = reactive<FormRules<transition>>({
+  name: [
+    { required: true, message: 'Please input Activity name', trigger: 'blur' },
+    { min: 3, max: 5, message: 'Length should be 3 to 5', trigger: 'blur' },
+  ],
+  region: [
+    {
+      required: true,
+      message: 'Please select Activity zone',
+      trigger: 'change',
+    },
+  ],
+  count: [
+    {
+      required: true,
+      message: 'Please select Activity count',
+      trigger: 'change',
+    },
+  ],
+  date1: [
+    {
+      type: 'date',
+      required: true,
+      message: 'Please pick a date',
+      trigger: 'change',
+    },
+  ],
+  date2: [
+    {
+      type: 'date',
+      required: true,
+      message: 'Please pick a time',
+      trigger: 'change',
+    },
+  ],
+  location: [
+    {
+      required: true,
+      message: 'Please select a location',
+      trigger: 'change',
+    },
+  ],
+  type: [
+    {
+      type: 'array',
+      required: true,
+      message: 'Please select at least one activity type',
+      trigger: 'change',
+    },
+  ],
+  resource: [
+    {
+      required: true,
+      message: 'Please select activity resource',
+      trigger: 'change',
+    },
+  ],
+  desc: [
+    { required: true, message: 'Please input activity form', trigger: 'blur' },
+  ],
+})
 
 const submitForm = async (formEl: FormInstance | undefined) => {
   if (!formEl) return
@@ -141,7 +148,10 @@ const submitForm = async (formEl: FormInstance | undefined) => {
   })
 }
 function getParams(){
-  console.log(props.transition.value)
+  
+console.log(props)
+console.log(props.params)
+  console.log(params.transition.value)
 }
 
 // const resetForm = (formEl: FormInstance | undefined) => {
