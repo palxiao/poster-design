12cb6d67ad1908247806efbc228bd82c4a36d000
1254107736@qq.com <125410736@qq.com>
2024-05-08 00:50:06 +0800

    2-翻页类型和预览

--- src/components/modules/layout/designBoard/hooks/useScroll.ts
+++ src/components/modules/layout/designBoard/hooks/useScroll.ts
@@ -1,4 +1,5 @@
-import {ref, reactive, Ref} from 'vue'
+import { log } from 'console';
+import {ref, reactive, Ref, onMounted} from 'vue'
 // import { useCanvasStore } from '@/store'
 // import { storeToRefs } from 'pinia'
 
@@ -15,38 +16,26 @@ export default function (dPage, dLayouts) {
   let page_type = ref('');
   let timeout = reactive('');
   let isDragging = ref(false);
+  let scrollContainer = ref('');
 
- 
-// 启动自动滚动
-  setTimeout(() => {
-    page_type.value = dPage.value.page_type
-    console.log(page_type)
-  // 获取滚动容器
-    var scrollContainer = document.getElementById('out-page');
-    console.log(scrollContainer)
-    scrollContainer.addEventListener('mousedown', function(e) {
-      isDragging.value = true;
-    });
-    scrollContainer.addEventListener('mouseup', function(e) {
-      isDragging.value = false;
-      // 鼠标停止后，再次启动的时间
-      // if(dPage.value.scrolldelay){
-        setTimeout(() => {
+  // 挂载后，如果为自动滚动时，进行自动滚动
+  onMounted(() => {
+    console.log(autoScroll.value)
+    if(autoScroll.value){
+      setTimeout(() => {
+        page_type.value = dPage.value.page_type
+        scrollContainer = document.getElementById('out-page')
+        // 长页自动滚动
+        if(page_type.value === 'longPage'){
+          console.log(page_type.value)
+
           fnAutoScroll(scrollContainer);
-        }, dPage.value.scrolldelay || 1e3);
-      // }
-    });
-    scrollContainer.addEventListener('mousemove', function(e) {
-      if (isDragging.value) {
-        // 拖动时滚动div
-        scrollContainer.scrollTop -= e.movementY * 2;
-        // 阻止默认的滚动行为
-        e.preventDefault();
-      } 
-    });
-    console.log(isDragging.value);
-    fnAutoScroll(scrollContainer);
-  }, 1000);
+        } else {
+          fnAutoTurnPage()
+        }
+      }, 1e3);
+    }
+  })
 
   // 自动滚动到底部
   function fnAutoScroll(element) {
@@ -57,7 +46,7 @@ export default function (dPage, dLayouts) {
     if(isDragging.value) {
       timeout = null;
     } else {
-      timeout = setTimeout(function() { fnAutoScroll(element); }, dPage.value.scrollSpeed || 1); // 递归调用，默认每10毫秒滚动一次，数字越大越慢，越小越快
+      timeout = setTimeout(function() { fnAutoScroll(element); }, dPage.value.scrollSpeed || 10); // 递归调用，默认每10毫秒滚动一次，数字越大越慢，越小越快
     }
   }
   
@@ -72,44 +61,57 @@ export default function (dPage, dLayouts) {
     }, autoSpeed)
   }
 
-  
-  // 监听鼠标滑动事件
-  function moveOver(e){
-    console.log(e);
-    
-  }
+
   // 监听鼠标滑动事件
   function mousedown({x, y}){
     console.log('mousedown---');
-    // 点击时，关闭自动滚动
-    clearInterval(autoScrollInterval);
-    console.log(y);
-    mouseDownY.value = y;
+    if(page_type.value === 'longPage'){
+      isDragging.value = true;
+    } else {
+      // 点击时，关闭自动滚动
+      clearInterval(autoScrollInterval);
+      console.log(y);
+      mouseDownY.value = y;
+    }
   }
   // 监听鼠标滑动事件
-  function mouseleave(e){
-    console.log('mousemove---');
-    console.log(e);
+  function mousemove(e){
+    if (isDragging.value && page_type.value === 'longPage') {
+      // 拖动时滚动div
+      scrollContainer.scrollTop -= e.movementY * 2;
+      // 阻止默认的滚动行为
+      e.preventDefault();
+    } 
   }
 
   // 监听鼠标滑动事件
   function mouseup({x, y}){
-    console.log('mouseup---');
-    mouseUpY.value = y;
-    console.log(y);
-    // 切换到上一页（向下滑）
-    if(mouseUpY.value - mouseDownY.value >= scrollTarget){
-      console.log('向下滑');
-      if(page_index.value <= 0) return 
-      page_index.value = page_index.value - 1;
-    }
-    // 切换到下一页（向上滑）
-    if(mouseDownY.value - mouseUpY.value >= scrollTarget){
-      console.log('向上滑');
-      if(page_index.value >= dLayouts.value.length - 1) return 
-      page_index.value = page_index.value + 1;
+    if(page_type.value === 'longPage'){
+      isDragging.value = false;
+      // 鼠标停止后，再次启动的时间
+      // if(dPage.value.scrolldelay){
+        setTimeout(() => {
+          fnAutoScroll(scrollContainer);
+        }, dPage.value.scrolldelay || 1e3);
+      // }
+    } else {
+      console.log('mouseup---');
+      mouseUpY.value = y;
+      console.log(y);
+      // 切换到上一页（向下滑）
+      if(mouseUpY.value - mouseDownY.value >= scrollTarget){
+        console.log('向下滑');
+        if(page_index.value <= 0) return 
+        page_index.value = page_index.value - 1;
+      }
+      // 切换到下一页（向上滑）
+      if(mouseDownY.value - mouseUpY.value >= scrollTarget){
+        console.log('向上滑');
+        if(page_index.value >= dLayouts.value.length - 1) return 
+        page_index.value = page_index.value + 1;
+      }
     }
   }
 
-  return {autoScroll, page_index, page_type, fnAutoScroll, fnAutoTurnPage, moveOver, mousedown, mouseleave, mouseup}
+  return {autoScroll, page_index, page_type, fnAutoScroll, fnAutoTurnPage, mousedown, mousemove, mouseup}
 }
